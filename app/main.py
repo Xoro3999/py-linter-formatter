47e3937fb2d5f47f79a8265d2a6bddac9ad314f0
Xoro3999 <xomoxomok@gmail.com>
2026-05-12 16:41:42 +0300

    f

--- app/main.py
+++ app/main.py
@@ -17,9 +17,6 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 
 
 def format_linter_report(linter_report: dict) -> list:
-    formatted_files = [
+    return [
         format_single_linter_file(file_path, errors)
         for file_path, errors in linter_report.items()
-    ]
-
-    return sorted(formatted_files, key=lambda x: x["path"])
